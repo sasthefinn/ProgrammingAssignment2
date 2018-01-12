6239682b4f6fff7d5e027bd87035166532967c27
sasthefinn <34423974+sasthefinn@users.noreply.github.com>
2018-01-11 19:41:59 -0600

    Added function bodies and comments
    Wrote function bodies and added appropriate comments to satisfy week 3 coursera programming assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Week 3 programming assignment
+## makeCacheMatrix creates a cached matrix object that caches the inverse of the input matrix
+## cacheSolve returns the inverse of the cached matrix, recalculating if necessary
 
-## Write a short comment describing this function
+## Store input matrix, and create cache object that includes methods to get and set the data
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) m <<- solve
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Retrieve inverse from cache object, calculating the inverse if it isn't set
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
 }
